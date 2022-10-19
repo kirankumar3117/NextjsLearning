1108664ff9569a13c1b6cad1c0f9922052f0a0bf
kiran kumar <ch.kirankumar311@gmail.com>
2022-10-19 12:33:47 +0530

    routing

---
+++ chapter1/pages/about.js
@@ -0,0 +1,5 @@
+function About(){
+    return(<h1>About World!</h1>)
+}
+
+export default About;
---
+++ chapter1/pages/profile.js
@@ -0,0 +1,5 @@
+function Profile(){
+    return(<h1>Profile World!</h1>)
+}
+
+export default Profile;
